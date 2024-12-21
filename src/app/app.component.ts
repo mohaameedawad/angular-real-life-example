aaf89ef0afd3b964db6dbe81d337e88930e09cf3
FLAIRSTECH\mohamed.awaad <mohamed.awaad@flairstech.com>
2024-12-21 17:53:01 +0200

    bug commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "./core/layout/header.component";
 import { RouterOutlet } from "@angular/router";
 import { FooterComponent } from "./core/layout/footer.component";
+import { MarkdownPipe } from "./shared/pipes/markdown.pipe";
 
 @Component({
   selector: "app-root",
@@ -9,4 +10,6 @@ import { FooterComponent } from "./core/layout/footer.component";
   standalone: true,
   imports: [HeaderComponent, RouterOutlet, FooterComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private MarkdownPipe: MarkdownPipe) {}
+}
